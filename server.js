e488b6701b81c90e0394e985a18e7f5a46252b23
James Fuller <jfuller957@gmail.com>
2019-09-11 21:00:44 -0700

    displaying data

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
-const path = require('path')
-const db = require('./db')
+const path = require('path');
+const db = require('./db')('./products.json');
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  next();
+})
 
 app.get('/', (req, res, next) =>{
   res.sendFile(path.join(__dirname, 'index.html'))
@@ -9,11 +15,12 @@ app.get('/', (req, res, next) =>{
 
 app.get('/api/products', async(req,res,next) => {
   try{
-    res.send(await db.findAll());
+    const data = await db.findAll();
+    res.send(data);
   }
   catch(ex){
     next(ex);
   }
-})
+});
 
-app.listen(3000, ()=> console.log('listening on port 3000'))
+app.listen(3000, ()=> console.log('listening on port 3000'));
