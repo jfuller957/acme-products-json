037e58096a166347ccd66565a3aa0ed5269c7f9d
James Fuller <jfuller957@gmail.com>
2019-09-11 21:19:26 -0700

    routes established

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const db = require('./db')('./products.json');
+const db = require('./db')('./products.json', (item, items)=> {
+  if(!item.name){
+    return "Product name required"
+  }
+});
 
 app.use(express.json());
 
@@ -21,6 +25,30 @@ app.get('/api/products', async(req,res,next) => {
   catch(ex){
     next(ex);
   }
+});
+
+app.post('/api/products', async(req, res, next) => {
+  try {
+    res.send(await db.create(req.body));
+  }
+  catch(ex) {
+    next(ex);
+  }
+});
+
+app.delete('/api/products/:id', async(req, res, next) => {
+  try {
+    await db.destroy(req.params.id);
+    res.sendStatus(204);
+  }
+  catch(ex) {
+    next(ex);
+  }
+});
+
+app.use((req, res, next)=> {
+  console.log(err);
+  res.status(500).send({ message: err.message})
 });
 
 app.listen(3000, ()=> console.log('listening on port 3000'));
